320342bac9c553174abbac0b61e4b50c166160b8
PandaPera <pandapera2020@gmail.com>
2021-03-08 20:19:42 +0100

    added avatar to registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,10 +2,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:author).permit(:username, :email, :password, :password_confirmation)
+    params.require(:author).permit(:avatar, :username, :email, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:author).permit(:username, :email, :password, :password_confirmation, :current_password)
+    params.require(:author).permit(:avatar, :username, :email, :password, :password_confirmation, :current_password)
   end
 end
