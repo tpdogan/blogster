ac000e2fe6a5c7b0d3ab0060ada0da470d00c0e5
PandaPera <pandapera2020@gmail.com>
2021-03-08 09:45:48 +0100

    Created articles controller and populated with basic CRUD methods

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,48 @@
+class ArticlesController < ApplicationController
+  before_action :authenticate_author!, except: [:index, :show]
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+  end
+  
+  private
+  
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+end
