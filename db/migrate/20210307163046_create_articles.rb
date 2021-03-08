a76b217d369d84f59809e9229928cf3e82fb9ad5
PandaPera <pandapera2020@gmail.com>
2021-03-08 07:57:21 +0100

    Bug fix for original column of articles migration

--- db/migrate/20210307163046_create_articles.rb
+++ db/migrate/20210307163046_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration[6.1]
     create_table :articles do |t|
       t.string :title
       t.text :body
-      t.original :text
+      t.text :original
 
       t.timestamps
     end
