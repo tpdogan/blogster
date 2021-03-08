68d588ef42c81f3dd62fcb7e46823672a4cd8666
PandaPera <pandapera2020@gmail.com>
2021-03-08 08:04:51 +0100

    validations for article is added

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ApplicationRecord
   belongs_to :author
   has_many :comments, as: :commentable
   has_many :tags, through: :article_to_tag
+
+  validates :title, :body, presence: true
+  validates :title, uniqueness: true
+  validates :body, length: { in: 150..500 }
 end
