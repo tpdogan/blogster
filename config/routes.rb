ace387a281d465523603cd2ae1da89daac41ec59
PandaPera <pandapera2020@gmail.com>
2021-03-08 07:49:32 +0100

    Added registrations controller for devise to permit username

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :authors
+  devise_for :authors, :controllers => { :registrations => 'registrations' }
   
   resources :articles do
     resources :comment
