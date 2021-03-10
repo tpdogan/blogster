1426d053ab373572d3b97ba70c845163a7a8433a
PandaPera <pandapera2020@gmail.com>
2021-03-10 04:15:20 +0100

    Article form and tagEditor is updated for tags during article editing

--- app/javascript/packs/tagEditor.js
+++ app/javascript/packs/tagEditor.js
@@ -1,5 +1,13 @@
-// Initialize tag array
-let tags = []
+// Initialize tag array by adding any existing tag (due to edit)
+let tags = document.getElementById('article_tags').value.split(', ')
+
+// Add events to remove tag buttons if any exist (due to edit)
+document.querySelectorAll('.editor__removeTag').forEach(item => {
+  item.addEventListener('click', () => {
+    removeTag(item.parentElement.textContent.trim())
+    item.parentElement.remove()
+  })
+})
 
 // Listen to input for tags to add
 const tagInput = document.getElementById('tag_input')
@@ -42,29 +50,35 @@ function createTag(tag) {
 
   // Create delete button
   const cross = document.createElement('i')
-  cross.className = 'fal fa-times'
-  cross.addEventListener('click', () => {
-    cross.parentElement.remove()
-    removeTag(tag)
+  cross.className = 'fal fa-times fa-sm'
+  const crossWrap = document.createElement('div')
+  crossWrap.className = 'editor__removeTag'
+  crossWrap.appendChild(cross)
+  crossWrap.addEventListener('click', () => {
+    removeTag(crossWrap.parentElement.textContent.trim())
+    crossWrap.parentElement.remove()
   })
-  newTag.appendChild(cross)
+  newTag.appendChild(crossWrap)
 
   // Add created tag to wrap
   const editorTags = document.querySelector('.editor__tags-wrap')
   editorTags.appendChild(newTag)
 }
 
+// Adds the tag to the array and input for form
 function addTag(tag) {
   tags.push(tag)
   listTags()
 }
 
+// Removes the tag from the array and input for form
 function removeTag(tag) {
   const index = tags.indexOf(tag)
   tags.splice(index, 1)
   listTags()
 }
 
+// Reports updated tags array to input for form
 function listTags() {
   document.getElementById('article_tags').value = tags.join(', ')
 }
