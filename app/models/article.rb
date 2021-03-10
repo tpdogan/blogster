6c587d98ca90c505da96eca3f1e06bd68402cbc8
PandaPera <pandapera2020@gmail.com>
2021-03-10 18:57:09 +0100

    dependent destroys are added to remove sub/comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :author
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
   
   has_many :article_to_tags
   has_many :tags, through: :article_to_tags
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :author
   belongs_to :commentable, polymorphic: true
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
 
   validates :body, presence: true, length: { maximum: 100 }
 end
